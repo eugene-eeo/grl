0866bfff54a1483200e9d2bcdebc8bb96987d638
Eugene Eeo <xe0jun@hotmail.com>
2014-09-18 21:51:39 +0800

    added diamond proximity function

--- memgraph/utils.py
+++ memgraph/utils.py
@@ -1,3 +1,6 @@
+from math import ceil
+
+
 def proximity(distance, start):
     sx, sy = start
     height = width = 1 + (2 * distance)
@@ -18,3 +21,30 @@ def diagonal(distance, start, gradient):
         x = sx + dx
         y = sy + (dx * gradient)
         yield x, y
+
+
+def diamond_proximity(distance, start):
+    sx, sy = start
+    height = 1 + (2 * distance)
+    middle = height // 2
+
+    min_x = sx - distance
+    min_y = sy - distance
+
+    width = 1
+    elems = [(min_x + dx) for dx in range(height)]
+    mid = len(elems) // 2
+
+    for dy in range(height):
+        y = min_y + dy
+        yield elems[mid], y
+
+        blocks = int((width - 1) / 2)
+        for delta in range(1, blocks + 1):
+            yield elems[mid+delta], y
+            yield elems[mid-delta], y
+
+        if (dy + 1) > middle:
+            width -= 2
+            continue
+        width += 2
