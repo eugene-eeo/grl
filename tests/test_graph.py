a6b51555db8b384cd5e4fbd0b684c9148294ef9d
Eugene Eeo <xe0jun@hotmail.com>
2014-09-24 18:04:55 +0800

    some tests

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -35,3 +35,10 @@ def test_size(graph):
 def test_values_of(graph):
     graph[0,1] = 0
     assert list(graph.values_of([(0,1)])) == [0]
+
+
+def test_values(graph):
+    for i in range(3):
+        graph[0,i] = i
+        graph[0,i] = i
+    assert list(graph.values()) == [0,1,2]
