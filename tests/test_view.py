89922148299bc81b84c298907d452983dbab4dd1
Eugene Eeo <xe0jun@hotmail.com>
2014-09-26 21:23:30 +0800

    test views

---
+++ tests/test_view.py
@@ -0,0 +1,21 @@
+def test_view_iter(graph):
+    for item in range(0, 3):
+        graph[0,item] = item
+
+    view = graph.view[:, 0:3]
+    assert list(view) == [(0,0), (0,1), (0,2)]
+
+
+def test_view_items(graph):
+    view = graph.view[1:, 0:3]
+    view[0,0] = 1
+    view[3,3] = 2
+
+    assert graph[1,0] == view[0,0] == 1
+    assert graph[4,3] == view[3,3] == 2
+
+    del view[0,0]
+    del view[3,3]
+
+    assert not graph[1,0]
+    assert not graph[4,3]
