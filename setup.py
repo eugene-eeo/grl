55ac3c87e1919ce2ee0a50d89f3d22a124adccea
Eugene Eeo <xe0jun@hotmail.com>
2014-09-28 15:12:25 +0800

    added setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='memgraph',
+      version='0.0.1',
+      description='In memory 2D graph',
+      author='Eugene Eeo',
+      author_email='packwolf58@gmail.com',
+      url='https://github.com/eugene-eeo/memgraph',
+      packages=['memgraph'],
+     )
