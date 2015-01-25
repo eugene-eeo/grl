06568315ffa8e68fe6edc04fd1461b3dfe7f5d46
Eugene Eeo <xe0jun@hotmail.com>
2015-01-25 13:34:52 +0800

    added README

---
+++ README.rst
@@ -0,0 +1,13 @@
+grl
+===
+
+grl is a library of utility functions for points. It
+includes functions for computing distances, whether a
+point is included in a given proximity to another
+point, etc.
+
+.. code-block:: python
+
+    import grl
+    assert grl.distance([0,1], [1,1]) == 1.0
+    assert [0,1] in grl.proximity(1, [0,0])
--- grl/core.py
+++ grl/core.py
@@ -3,14 +3,14 @@ from math import sqrt
 
 def proximity(distance, start):
     sx, sy = start
-    height = width = 1 + (2 * distance)
+    height = 1 + (2 * distance)
 
     min_x = sx - distance
     min_y = sy - distance
 
     for dx in range(width):
         x = min_x + dx
-        for dy in range(height):
+        for dy in range(width):
             y = min_y + dy
             yield x, y
 
