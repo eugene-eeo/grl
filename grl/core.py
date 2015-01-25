347403210fc6e31385cc036a9563497d527d0771
Eugene Eeo <xe0jun@hotmail.com>
2015-01-25 12:26:17 +0800

    more readable code

--- grl/core.py
+++ grl/core.py
@@ -50,5 +50,5 @@ def distance(p1, p2):
         return 0.0
     x1, y1 = p1
     x2, y2 = p2
-    return sqrt(pow(x1 - x2, 2) +
-                pow(y1 - y2, 2))
+    return sqrt((x1 - x2)**2 +
+                (y1 - y2)**2)
